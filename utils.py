c6e9b019399b6582b31a009c33cce13ae0ecf536
Mahmoud Wahdan <mahmoud.a.wahdan@gmail.com>
2020-07-31 23:06:49 +0400

    support boolean parameter in argparse

--- utils.py
+++ utils.py
@@ -4,6 +4,21 @@
 """
 
 from itertools import chain
+import argparse
+
+
+def str2bool(v):
+    """
+    Source: https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse/43357954#43357954
+    """
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def flatten(y):
